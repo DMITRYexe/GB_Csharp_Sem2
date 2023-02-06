7815e1cb6bc68c0390321928388db87a48249f0f
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-06 16:57:12 +0300

    Homework 5. Palindrome checking

--- Homework/Project4_QuartersAndCoordinates/Program.cs
+++ Homework/Project4_QuartersAndCoordinates/Program.cs
@@ -1,8 +1,7 @@
 ﻿//24. Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
 Console.WriteLine("Привет! Данная программа определяет четверть оси координат по введеным Х и У");
 Console.WriteLine("При этом, Х и У отличны от нуля");
-Console.WriteLine("Введите координату Х, затем координату У");
-double x = Convert.ToDouble(Console.ReadLine()), y =  Convert.ToDouble(Console.ReadLine());
+double x = -5.5, y =  5.5; // нет требования ввода с клавиатуры
 bool x1 = true, y1 = true; // интересно реализовать это через bool (хотя можно и без)
 if (x<0) x1 = false;
 if (y<0) y1 = false;
