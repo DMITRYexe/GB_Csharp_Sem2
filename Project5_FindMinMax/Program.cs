eb0976d30fd14743fdbdffa56ce60b370b9f72fe
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-06 03:55:26 +0300

    Project 5. Finding min and max digits

---
+++ Project5_FindMinMax/Program.cs
@@ -0,0 +1,12 @@
+﻿// Ищуем минимальную и максимальную цифры в числе
+
+int i = 1354, max = -1, min = 10, digit; // в мин и макс указываем числа выше и ниже диапазаона соответсвенно
+while (i != 0)
+{
+  digit = i % 10;
+  if (digit > max) max = digit;
+  if (digit < min) min = digit;
+  i /= 10;
+}
+Console.WriteLine($"Max={max}, min={min}");
+
