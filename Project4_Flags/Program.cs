bda7880868ceed8093a65ce204e10aec4b108541
Dmitry Korotchuk <dmitrykorotchuk@mail.ru>
2023-02-06 03:40:58 +0300

    Project 4. Flag

---
+++ Project4_Flags/Program.cs
@@ -0,0 +1,22 @@
+﻿// Работа с флагами
+// Дано число. Проверить если в нем "0".
+int i = 13450;
+bool flag = false;
+while (i != 0)
+{
+  int d = i % 10;
+  if (d == 0)
+  {
+    flag = true;
+  }
+  i /= 10;
+}
+
+if (flag)  // запись аналогична flag=true;
+{
+  Console.WriteLine("В числе есть 0");
+}
+else
+{
+  Console.WriteLine("В числе нет 0");
+}
